91d56d1fa278e73fabf1d9d94cf7cd962d828d0c
Ashleigh <ashleigh.amrine@gmail.com>
2014-03-06 20:28:53 -0500

    Added Inflewenza

---
+++ Games/Inflewenza/Assets/EnableComponents.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableComponents : MonoBehaviour
+{
+    private UIPanel myLight;
+    
+    
+    void Start ()
+    {
+        myLight = GetComponent<UIPanel>();
+    }
+    
+    
+    void Update ()
+    {
+        if(Input.GetButtonDown("buttonY"))
+        {
+			Time.timeScale = 0;
+            myLight.enabled = true;
+        }
+		if(Input.GetButtonDown("buttonB"))
+        {
+			Time.timeScale = 1;
+            myLight.enabled = false;
+        }
+    }
+}
---
+++ Games/Inflewenza/Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	void update(){
+		 
+		Debug.Log("Exit was clicked!");
+		
+		
+	}
+		
+	void OnStartClicked(){
+		Application.LoadLevel("airplane_scene");
+			}
+		
+
+	void OnExitClicked(){
+		Application.LoadLevel("Main");
+	}
+}
