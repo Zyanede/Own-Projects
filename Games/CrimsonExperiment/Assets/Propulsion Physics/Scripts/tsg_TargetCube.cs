554f8ff51f1cacddc50c204de94f38a9a218b63c
Ashleigh Amrine <ashleigh.amrine@gmail.com>
2015-05-07 21:32:45 -0400

    Adding pages

--- Games/CrimsonExperiment/Assets/Propulsion Physics/Scripts/tsg_TargetCube.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class tsg_TargetCube : MonoBehaviour {
-  void Start() {
-    if(Application.isPlaying) {
-      gameObject.SetActive(false);
-    }
-  }
-}
--- Games/CrimsonExperiment/Assets/Resources/Scripts/CameraFollow.js
+++
@@ -1,8 +0,0 @@
-#pragma strict
-var Player : GameObject;
- 
-function Update(){
-    transform.position.x= Player.transform.position.x;
-    transform.position.y= Player.transform.position.y+1.5;
-    
-}
